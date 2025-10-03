8a860b3b5bb689c9cc7b5c78de63d947c7140838
Rotsiniaina <randriantsimbanavalona@gmail.com>
2025-10-03 10:27:13 +0200

    gaulois et romains

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -2,4 +2,22 @@ package personnages;
 
 public class Romain {
 
+	private String nom;
+	private int force;
+	private Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println((prendreParole() + "\"" + texte + "\"");
+	}
+	
+	private String prendreParole() {
+		return  "Le romain " + nom + " : ";
+	}
+	
+
 }
