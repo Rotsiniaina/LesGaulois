9bd581dc9c2bd7e4c88944b6e31dbb3effe7be00
Rotsiniaina <randriantsimbanavalona@gmail.com>
2025-10-03 13:00:51 +0200

    ajout de testGaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,6 +3,7 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
+	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
@@ -17,8 +18,20 @@ public class Gaulois {
 	private String prendreParole() {
 		return "Le gaulois" + nom + " : ";
 	}
+	
+	public static void main(String[] args) {
+		Gaulois asterix= new Gaulois("Astérix", 8);
+		System.out.println(asterix.getNom());
 	}
 	
-
-
+	@Override
+	public String toString() {
+	    return nom;
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom +  " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+		
+	}
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,20 +4,35 @@ public class Romain {
 
 	private String nom;
 	private int force;
-	private Romain(String nom, int force) {
+	
+	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
+	
 	public String getNom() {
 		return nom;
 	}
+	
+	@Override
+	public String toString() {
+	    return nom;
+	}
+	
 	public void parler(String texte) {
-		System.out.println((prendreParole() + "\"" + texte + "\"");
+		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
 	
 	private String prendreParole() {
 		return  "Le romain " + nom + " : ";
 	}
 	
+	public void recevoirCoup(int forceCoup) {
+		force= force - forceCoup;
+		if (force<1)
+			parler("J'abandonne!");
+		else
+			parler("Aie");	
+	}
 
 }
