c9313bf0136f99bf58151ea50b3ff4ea28c11253
Rotsiniaina <randriantsimbanavalona@gmail.com>
2025-10-07 19:44:31 +0200

    ajout de druide et chaudron

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -3,13 +3,14 @@ package personnages;
 public class Druide {
 	private String nom;
 	private int force;
+	private Chaudron chaudron;
 	
 	public String getNom() {
 		return nom;
 	}
 
 	private String prendreParole() {
-		return "Le Druide" + nom + " : ";
+		return "Le Druide " + nom + " : ";
 	}
 	
 	public void parler(String texte) {
@@ -18,19 +19,19 @@ public class Druide {
 
 	public void fabriquerPotion(int quantite,int forcePotion) {
 		remplirChaudron(quantite,forcePotion);
-		parler("J'ai concoté" + quantite + "doses de potion magique.Elle a une force de" + forcePotion + ".");
+		parler("J'ai concoté " + quantite + " doses de potion magique.Elle a une force de " + forcePotion + ".");
 	}
 	
 	public void booster(Gaulois gaulois) {
 		if (resterPotion){
 			if (gaulois.getNom() == "Obelix")
-				parler("Non," + gaulois.getNom() + "Non!...Et tu le sais très bien!");
+				parler("Non, " + gaulois.getNom() + " Non!...Et tu le sais très bien!");
 			else
 				prendreLouche();
 				boirePotion(forcePotion);
 				parler("Tiens" + gaulois.getNom() + "un peu de potion magique.");
 		} else
-			parler("Désolé" + gaulois.getNom() + "il n'y a plus de une seule goutte de potion");
+			parler("Désolé " + gaulois.getNom() + " il n'y a plus de une seule goutte de potion");
 	}
 	
 	
