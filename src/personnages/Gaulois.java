d9392c7a200d0202761487b7bd98b63c63e4619e
Rotsiniaina <randriantsimbanavalona@gmail.com>
2025-11-25 11:33:06 +0100

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,45 +1,57 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Gaulois {
     private String nom;
     private int force;
-    private Village village; // peut être null si pas encore dans un village
+    private int effetPotion = 1;
+    private Village village;
 
     public Gaulois(String nom, int force) {
         this.nom = nom;
         this.force = force;
     }
 
-    public String getNom() {
-        return nom;
-    }
-
     public void setVillage(Village village) {
         this.village = village;
     }
 
-    public void parler(String texte) {
-        System.out.println(prendreParole() + "« " + texte + " »");
+    public Village getVillage() {
+        return village;
     }
 
-    private String prendreParole() {
-        return "Le gaulois " + nom + " : ";
+    public String getNom() {
+        return nom;
+    }
+
+    public void parler(String texte) {
+        System.out.println("Le gaulois " + nom + " : « " + texte + " »");
     }
 
     public void frapper(Romain romain) {
+        int forceCoup = (force * effetPotion) / 3;
         System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-        romain.recevoirCoup(force / 3);
+        romain.recevoirCoup(forceCoup);
+        if (effetPotion > 1)
+            effetPotion--;
+    }
+
+    public void boirePotion(int forcePotion) {
+        effetPotion = forcePotion;
     }
 
     public void sePresenter() {
         System.out.print("Le Gaulois " + nom + " : ");
-
         if (village != null) {
-            System.out.println("\"Bonjour, je m'appelle " + nom +
-                               ". J'habite le village " + village.getNom() + ".\"");
+            System.out.println("\"Bonjour, je m'appelle " + nom + ". J'habite le village " + village.getNom() + ".\"");
         } else {
-            System.out.println("\"Bonjour, je m'appelle " + nom +
-                               ". Je voyage de villages en villages.\"");
+            System.out.println("\"Bonjour, je m'appelle " + nom + ". Je voyage de villages en villages.\"");
         }
+    }
+
+    @Override
+    public String toString() {
+        return nom;
     }
 }
