f6d34d6d3865309b0fc8217175ddc9c5c83bcc4a
Rotsiniaina <randriantsimbanavalona@gmail.com>
2025-11-25 11:29:11 +0100

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,68 +1,45 @@
 package personnages;
 
 public class Gaulois {
-	private String nom;
-	private int force;
-	private int effetPotion = 1;
-	private Village village;
+    private String nom;
+    private int force;
+    private Village village; // peut être null si pas encore dans un village
 
-	public Gaulois(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
-	}
-	
-	public void setVillage(Village village) {
-	    this.village = village;
-	}
+    public Gaulois(String nom, int force) {
+        this.nom = nom;
+        this.force = force;
+    }
 
-	public Village getVillage() {
-	    return village;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    public void setVillage(Village village) {
+        this.village = village;
+    }
 
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-	}
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "« " + texte + " »");
+    }
 
-	private String prendreParole() {
-		return "Le gaulois" + nom + " : ";
-	}
+    private String prendreParole() {
+        return "Le gaulois " + nom + " : ";
+    }
 
-	public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("Astérix", 8);
-		System.out.println(asterix.getNom());
-	}
+    public void frapper(Romain romain) {
+        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+        romain.recevoirCoup(force / 3);
+    }
 
-	@Override
-	public String toString() {
-		return nom;  
-	}
+    public void sePresenter() {
+        System.out.print("Le Gaulois " + nom + " : ");
 
-	public void frapper(Romain romain) {
-		int forceCoup = (force * effetPotion) / 3;
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(forceCoup);
-		if (effetPotion > 1)
-			effetPotion--;
-	}
-
-	public void boirePotion(int forcePotion) {
-		effetPotion = forcePotion;
-	}
-
-	public void sePresenter() {
-		System.out.print("Le Gaulois " + nom + " : ");
-		if (this instanceof Chef) {
-			System.out.println("\"Bonjour, je m'appelle " + nom + ". Je suis le chef du village "
-					+ ((Chef) this).getVillage().getNom() + ".\"");
-		} else if (village != null) {
-			System.out.println("\"Bonjour, je m'appelle " + nom + ". J'habite le village " + village.getNom() + ".\"");
-		} else {
-			System.out.println("\"Bonjour, je m'appelle " + nom + ". Je voyage de villages en villages.\"");
-		}
-	}
-
+        if (village != null) {
+            System.out.println("\"Bonjour, je m'appelle " + nom +
+                               ". J'habite le village " + village.getNom() + ".\"");
+        } else {
+            System.out.println("\"Bonjour, je m'appelle " + nom +
+                               ". Je voyage de villages en villages.\"");
+        }
+    }
 }
