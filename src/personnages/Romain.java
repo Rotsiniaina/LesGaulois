09c5a51c46e32b69ffe07f68a0d14923d5d7401b
Rotsiniaina <randriantsimbanavalona@gmail.com>
2025-11-25 11:33:35 +0100

    Update Romain.java

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,13 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
+
     private String nom;
     private int force;
+    private Equipement[] equipements = new Equipement[2];
+    private int nbEquipement = 0;
 
     public Romain(String nom, int force) {
         this.nom = nom;
@@ -17,12 +22,52 @@ public class Romain {
         System.out.println("Le romain " + nom + " : « " + texte + " »");
     }
 
-    public void recevoirCoup(int forceDuCoup) {
-        force -= forceDuCoup;
-        if (force > 0) {
-            parler("Aïe !");
+    private boolean isInvariantVerified() {
+        return force >= 0;
+    }
+
+    public void recevoirCoup(int forceCoup) {
+        assert forceCoup > 0 : "Erreur : la force du coup doit être positive !";
+        int ancienneForce = force;
+        force -= forceCoup;
+        if (force < 1) {
+            force = 0;
+            parler("J'abandonne !");
         } else {
-            parler("J’abandonne…");
+            parler("Aïe");
         }
+        assert force <= ancienneForce : "Erreur : la force du Romain n’a pas diminué après le coup !";
+        assert isInvariantVerified() : "Erreur : invariant violé, la force est négative !";
+    }
+
+    public void sEquiper(Equipement equipement) {
+        switch (nbEquipement) {
+            case 2:
+                System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+                break;
+            case 1:
+                if (equipements[0] == equipement) {
+                    System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+                } else {
+                    ajouterEquipement(equipement);
+                }
+                break;
+            case 0:
+                ajouterEquipement(equipement);
+                break;
+            default:
+                System.out.println("Erreur inattendue dans le nombre d’équipements !");
+        }
+    }
+
+    private void ajouterEquipement(Equipement equipement) {
+        equipements[nbEquipement] = equipement;
+        nbEquipement++;
+        System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+    }
+
+    @Override
+    public String toString() {
+        return nom;
     }
 }
